378676cbef931f9fb6c1eb6a08e07b9ed36eac73
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2022-03-28 16:31:12 +0200

    add middleware to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message : 'Request received'})
-})
+app.use((req, res, next) =>{
+    console.log('Request received!');
+    next();
+});
+
+app.use((req, res, next) =>{
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({ message : 'Request received'});
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Response successfully received')
+    next();
+});
 
 module.exports = app;
