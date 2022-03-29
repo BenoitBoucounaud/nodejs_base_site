cf22f80e3a4d513d56470210683ed74527849025
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2022-03-29 14:46:46 +0200

    update router and create controllers

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,45 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const Thing = require('../models/thing');
+const stuffCtrl = require('../controllers/stuff');
 
-// put POST middlewares before GET requests (but write with 'use') to catch POST requests and not allow them to touch GET middlewares
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body // '...' is used to copy all elements from 'req.body'
-    });
-    thing.save() // return a Promise
-        .then(() => res.status(201).json({ message: 'Object saved'}))
-        .catch(() => res.status(400).json({error}))
-});
-
-// catch one thing
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({_id: req.params.id})
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(400).json({ error }))
-});
-
-// update one thing
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-        .then(() => res.status(200).json({ message : 'Object updated'}))
-        .catch(() => res.status(400).json({error}));
-});
-
-// delete one thing
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-        .then(() => res.status(200).json({message: 'Object deleted'}))
-        .catch(() => res.status(400).json({ error }));
-});
-
-// catch all things
-router.get('/', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
+router.get('/', stuffCtrl.getAllThings);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getThing);
+router.put('/:id', stuffCtrl.updateThing);
+router.delete('/:id', stuffCtrl.deleteThing);
 
 module.exports = router;
